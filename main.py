37b9e7f49c87152b6271d21a95e7b00b1b6bc9ca
ThanushaBai <thanushabai12@gmail.com>
2025-10-25 20:23:03 +0530

    initial commit

--- main.py
+++ main.py
@@ -4,20 +4,20 @@
 #continue
 #pass
 
-while True:
-    name = input("What is your name?")
-    if name != "":
-        break
+# while True:
+#     name = input("What is your name?")
+#     if name != "":
+#         break
 
 phone_number = "123-456-7890"
 for i in phone_number:
-    if i == "-":
+    if i == "*":
         continue
     print(i,end="")
-
-for i in range(1,21):
-
-    if i == 12:
-        pass
-    else:
-        print(i)
+#
+# for i in range(1,21):
+#
+#     if i == 12:
+#         pass
+#     else:
+#         print(i)
