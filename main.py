c34bd69bf9611e755dfb0beccd68a934be79c921
ThanushaBai <thanushabai12@gmail.com>
2025-10-26 14:56:56 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #function = block of code which is executed only when it is called
 
 
-def hello(name):
-    print("hello!"+name)
-    print("have a nice day!")
-
-my_name = "tc"
-hello(my_name)
-
-
-
-# def hello(firstname,lastname,age):
-#     print("hello "+firstname+" "+lastname)
-#     print("you are "+str(age)+" years old")
-#     print("have a nice day")
+# def hello(name):
+#     print("hello!"+name)
+#     print("have a nice day!")
 #
-# hello("t","c",21)
+# my_name = "tc"
+# hello(my_name)
+
+
+
+def hello(firstname,lastname,age):
+    print("hello "+firstname+" "+lastname)
+    print("you are "+str(age)+" years old")
+    print("have a nice day")
+
+hello("t","c",21)
